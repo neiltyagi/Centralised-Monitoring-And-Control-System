8d7ddf5eb2de84ccc74085caab8013f762dc104f
root <root@techmonk.techmonk.com>
2020-01-22 05:29:19 -0500

    dependecy script

---
+++ dependencyclient.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update
+apt install python3 gcc python3-dev python3-pip -y
+pip3 install setuptools
+pip3 install colorama
+pip3 install prettytable
+pip3 install psutil
+pip3 install  python-crontab
+
